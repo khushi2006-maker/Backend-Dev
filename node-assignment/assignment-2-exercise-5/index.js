5caefbb19228778c7b14c19078194f30aae25c4b
khushi2006-maker <khushisikarwar0022@gmail.com>
2026-01-22 15:09:41 +0530

    assignment 5: event loop execution order

---
+++ node-assignment/assignment-2-exercise-5/index.js
@@ -0,0 +1,17 @@
+// event loop ka order dikhane ke liye
+
+console.log("start");
+
+setTimeout(() => {
+    console.log("timeout");
+}, 0);
+
+Promise.resolve().then(() => {
+    console.log("promise");
+});
+
+process.nextTick(() => {
+    console.log("next tick");
+});
+
+console.log("end");

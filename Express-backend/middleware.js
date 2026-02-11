23eae614d7b0bed49f6d75c58d0f880993dfe1b7
khushi2006-maker <khushisikarwar0022@gmail.com>
2026-02-11 10:51:55 +0530

    Express-backend

---
+++ Express-backend/middleware.js
@@ -0,0 +1,71 @@
+const fs = require("fs").promises;
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const PORT = 8000;
+
+
+
+app.use((req, res, next) => {
+  const log = `\nRequest at: ${new Date().toLocaleString()} | Method: ${req.method} | URL: ${req.url}`;
+  
+  fs.appendFile("Log.txt", log)
+    .catch(err => console.error(err));
+
+  next();
+});
+
+
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers["authorization"];  
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Token missing. Access denied"
+    });
+  }
+
+  if (token === "mysecrettoken") {
+    console.log("User authenticated");
+    next(); 
+  } else {
+    return res.status(403).json({
+      message: "Invalid token"
+    });
+  }
+};
+
+
+
+app.use((req, res, next) => {
+  console.log("I am middleware 1");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("I am middleware 2");
+  next();
+});
+
+
+
+app.get("/dashboard", authMiddleware, (req, res) => {
+  res.json({
+    message: "Welcome! You are authenticated 🎉"
+  });
+});
+
+
+
+app.get("/", (req, res) => {
+  res.send("Public route — no auth needed");
+});
+
+
+
+app.listen(PORT, () => {
+  console.log("Server is listening on port:8000");
+});
